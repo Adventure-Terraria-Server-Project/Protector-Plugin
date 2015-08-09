1e20de82db1591c0db00d55cec1d412d96caf587
CoderCow <CoderCow@gmx.de>
2015-08-09 02:01:30 +0200

    API 1.21 compatibility.

--- Implementation/ProtectorPlugin.cs
+++ Implementation/ProtectorPlugin.cs
@@ -15,7 +15,7 @@ using Terraria.Plugins.Common;
 using Terraria.Plugins.Common.Hooks;
 
 namespace Terraria.Plugins.CoderCow.Protector {
-  [ApiVersion(1, 20)]
+  [ApiVersion(1, 21)]
   public class ProtectorPlugin: TerrariaPlugin, IDisposable {
     #region [Constants]
     private const string TracePrefix = @"[Protector] ";
