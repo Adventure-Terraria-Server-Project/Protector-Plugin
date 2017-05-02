c345d521338bba91cadc9d216d0698af52b9b00c
CoderCow <CoderCow@gmx.de>
2017-05-03 00:49:23 +0200

    chore(publishing): improve publishing script
    Some refactoring and added XenForo update functionality.

--- publish.ps1
+++ publish.ps1
@@ -2,7 +2,8 @@
 # Terraria Plugin Publishing Script, CoderCow 2017
 #
 # 
-# This script performs the following actions:
+# Actions Performed by this Script
+# 
 #   * Uses GitVersion to increment a component of the version number in the project's AssemblyInfo.cs.
 #     The component of the SemVer version number to increase (major, minor or patch) is determined by 
 #     the commits done since the last tag (BREAKING, feature or fix commits).
@@ -13,6 +14,11 @@
 #     also pushes this commit and the tag.
 #   * Uses GitReleaseManager to create a GitHub release and attaches the zip file to it, using the
 #     generated changelog as the description text.
+#   * Performs a login on https://tshock.co and edits the related plugin resource to update its
+#     version information.
+#   * Converts the repository's README.md to html and uses it as the description text for the tshock
+#     resource, so that descriptions remain consistent.
+#   * Adds a resource update to tshock.co using using the converted changelog.md as description.
 #
 # Usage Notes
 #
@@ -29,11 +35,14 @@
 #
 # Installing Dependencies
 #
-#   If you have chocolatey, you can install all of the required dependencies with:
+#   If you have chocolatey, most of the required dependencies can be installed by:
 #     choco install GitReleaseManager.Portable GitVersion.Portable 7zip rust -y & cargo install clog-cli
 # 
 #   Note that cargo is the bundled package manager of rust.
 #   You may have to register some of the binaries in your PATH.
+#
+#   To install the markdown powershell module, see this: 
+#   https://social.technet.microsoft.com/wiki/contents/articles/30591.convert-markdown-to-html-using-powershell.aspx
 #
 # Further Notes
 # 
@@ -45,6 +54,8 @@
 #   GitReleaseManager docs: http://gitreleasemanager.readthedocs.io
 
 $ErrorActionPreference = "Stop"
+
+Import-Module PowershellMarkdown
 
 $outDir = "$PSScriptRoot\bin\Release"
 $assemblyInfoPath = "$PSScriptRoot\Properties\AssemblyInfo.cs"
@@ -63,6 +74,12 @@ $outZipFileNameFormat = "Protector_{0}_API_{2}.zip"
 $gitHubUser = "CoderCow"
 $gitHubRepoOwner = "CoderCow"
 $gitHubRepoName = "Protector-Plugin"
+
+# information used to update the ressource on tshock.co
+$tshockUser = "CoderCow@gmx.de"
+$tshockResourceUri = "https://tshock.co/xf/index.php?resources/protector.190/"
+# for the XenForo description text
+$readmeFile = "$PSScriptRoot\README.md"
 
 $binariesToPublish = @(
   "$outDir\$targetName.dll",
@@ -93,9 +110,14 @@ function Main {
   Package-Files $outZipFile
 
   Create-Commit $releaseVersion $terrariaVersion $pluginApiVersion
-  Create-GitHubRelease $releaseVersion $outChangelogFile $outZipFile
 
+  Write-Host "Publishing to GitHub..."
+  Create-GitHubRelease $releaseVersion $outChangelogFile $outZipFile
   Start-Process "https://github.com/$gitHubUser/$gitHubRepoName/releases"
+
+  Write-Host "Updating TShock resource..."
+  Update-TShockResource $releaseVersion $terrariaVersion $pluginApiVersion $outChangelogFile
+  Start-Process "$tshockResourceUri/updates"
 }
 
 function Get-ApiVersion {
@@ -127,12 +149,7 @@ function Update-AssemblyVersion {
   GitVersion.exe /updateassemblyinfo $assemblyInfoPath | ConvertFrom-Json
 }
 
-function Generate-Changelog {
-  $pluginApiVersion = $args[0]
-  $tshockVersion = $args[1]
-  $terrariaVersion  = $args[2]
-  $outChangelogFile = $args[3]
-
+function Generate-Changelog($pluginApiVersion, $tshockVersion, $terrariaVersion, $outChangelogFile) {
   if (Test-Path $outChangelogFile) {
     Remove-Item -Force $outChangelogFile
   }
@@ -158,8 +175,7 @@ function Generate-Changelog {
   }
 }
 
-function Package-Files {
-  $outZipFile = $args[0]
+function Package-Files($outZipFile) {
   $outPluginDir = "$outDir\ServerPlugins"
   $outTShockDir = "$outDir\tshock"
 
@@ -172,11 +188,7 @@ function Package-Files {
   7z.exe a -y -r -bd -tzip -mx9 $outZipFile $outPluginDir $outTShockDir > $null
 }
 
-function Create-Commit {
-  $releaseVersion = $args[0]
-  $terrariaVersion = $args[1]
-  $pluginApiVersion = $args[2]
-
+function Create-Commit($releaseVersion, $terrariaVersion, $pluginApiVersion) {
   $tagName = $tagNameFormat -f $releaseVersion,$terrariaVersion,$pluginApiVersion
   $commitMessage = $commitMessageFormat -f $releaseVersion,$terrariaVersion,$pluginApiVersion
 
@@ -185,11 +197,7 @@ function Create-Commit {
   git tag --annotate $releaseVersion --message $tagName
 }
 
-function Create-GitHubRelease {
-  $releaseVersion = $args[0]
-  $outChangelogFile = $args[1]
-  $outZipFile = $args[2]
-
+function Create-GitHubRelease($releaseVersion, $outChangelogFile, $outZipFile) {
   $gitHubPassword = Read-Host "Enter password for GitHub user $gitHubUser"
 
   # This ensures that errors can be seen if they happen
@@ -198,6 +206,98 @@ function Create-GitHubRelease {
   git push origin --follow-tags
   GitReleaseManager.exe create -u $gitHubUser -p $gitHubPassword -o $gitHubRepoOwner -r $gitHubRepoName -n $releaseVersion -i $outChangelogFile -a $outZipFile
   GitReleaseManager.exe publish -u $gitHubUser -p $gitHubPassword -o $gitHubRepoOwner -r $gitHubRepoName -t $releaseVersion
+}
+
+function Update-TShockResource($releaseVersion, $terrariaVersion, $pluginApiVersion, $changelogFile) {
+  $tshockPassword = Read-Host "Enter password for TShock XenForo user $tshockUser"
+
+  # Invoke-WebRequest: https://msdn.microsoft.com/powershell/reference/5.1/microsoft.powershell.utility/Invoke-WebRequest
+  # FormObject: https://msdn.microsoft.com/en-us/library/microsoft.powershell.commands.formobject(v=vs.85).aspx
+
+  # authenticate
+  $response = Invoke-WebRequest -Uri "https://tshock.co/xf/index.php?login" -SessionVariable session
+  $formObject = $response.Forms["pageLogin"]
+  $formObject.Fields["login"] = $tshockUser
+  $formObject.Fields["password"] = $tshockPassword
+  $response = Invoke-WebRequest -Uri "https://tshock.co/xf/index.php?login/login" -Method Post -Body $formObject -WebSession $session
+
+  # if this causes a 403 then the login has probably failed
+  $response = Invoke-WebRequest -Uri "$tshockResourceUri/edit" -WebSession $session
+
+  # form in that page has no id, so finding it is a bit of a hassle
+  $formHtmlElement = $response.ParsedHtml.IHTMLDocument3_getElementsByTagName("form") | Where { $_.action.EndsWith("/save") }
+  $fields = Construct-FormFields $response $formHtmlElement
+
+  # can't set the version prefix directly, need a prefix id instead
+  # <optgroup label="Version">
+  $prefixOptionsList = $response.ParsedHtml.IHTMLDocument3_getElementsByTagName("optgroup") | Where { $_.label -eq "Version" }
+  # <option value="6" data-css="prefix prefixRed" >1.18 (obsolete)</option>
+  $prefixOption = $prefixOptionsList.getElementsByTagName("option") | Where { $_.innerText -eq $pluginApiVersion }
+  if ($prefixOption) {
+    $prefixId = $prefixOption.value
+    $fields["prefix_id"] = $prefixId
+  } else {
+    Write-Host "Looks like there's yet no prefix for API $pluginApiVersion available." -ForegroundColor Cyan
+    Write-Host "Keeping the current prefix for now." -ForegroundColor Green
+  }
+
+  # this is actually "API Version"
+  $fields["custom_fields[tshockver]"] = $pluginApiVersion
+  $fields["custom_fields[tshockversion]"] = $tshockVersion
+  $fields["version_string"] = $releaseVersion
+
+  $readmeMarkdown = Get-Content $readmeFile
+  $readmeMarkdown = ($readmeMarkdown -join "`n")
+  $readmeMarkdown = $readmeMarkdown -replace "<","&lt;"
+  $readmeMarkdown = $readmeMarkdown -replace ">","&gt;"
+  $readmeHtml = ConvertFrom-Markdown -MarkdownContent ($readmeMarkdown -join "`n")
+  # make things a bit prettier because XenForo does some weird transformations to the html
+  $readmeHtml = $readmeHtml -replace "<h3>","<br><h3>"
+  $readmeHtml = $readmeHtml -replace "</p>[\s\n\r]*<p>","<br><br>"
+  $fields["message_html"] = $readmeHtml
+
+  # save the resource
+  $response = Invoke-WebRequest -Uri "$tshockResourceUri/save" -Method Post -Body $fields -WebSession $session
+
+  # open "Post Resource Update" page
+  $response = Invoke-WebRequest -Uri "$tshockResourceUri/add-version" -WebSession $session
+  $formHtmlElement = $response.ParsedHtml.IHTMLDocument3_getElementsByTagName("form") | Where { $_.action.EndsWith("/save-version") }
+  
+  $fields = Construct-FormFields $response $formHtmlElement
+  $fields["download-url"] = "https://github.com/$gitHubUser/$gitHubRepoName/releases/tag/$releaseVersion"
+  $fields["version-string"] = $releaseVersion
+  $fields["title"] = "$releaseVersion Update"
+  $fields["message_html"] = "$releaseVersion Update"
+
+  $changelogMarkdown = Get-Content $changelogFile
+  # remove commit hashes
+  $changelogMarkdown = $changelogMarkdown -replace "\(\[[A-Za-z0-9]+\]\([A-Za-z0-9]+\)\)",""
+
+  $changelogHtml = ConvertFrom-Markdown -MarkdownContent ($changelogMarkdown -join "`n")
+  # make things a bit prettier because XenForo does some weird transformations to the html
+  $changelogHtml = $changelogHtml -replace "<h4>","<br><h4>"
+  $fields["message_html"] = $changelogHtml
+
+  # post update
+  $response = Invoke-WebRequest -Uri "$tshockResourceUri/save-version" -Method Post -Body $fields -WebSession $session
+}
+
+function Construct-FormFields($request, $formHtmlElement) {
+  $fields = @{}
+  $inputFields = $formHtmlElement.getElementsByTagName("input") | Where { $_.name -and $_.type -ne "button" }
+  foreach ($newField in $inputFields) {
+    $fields[$newField.name] = $newField.Value 
+  }
+
+  foreach ($newField in $formHtmlElement.getElementsByTagName("textarea")) {
+    $fields.Add($newField.name, $newField.Value)
+  }
+
+  foreach ($newField in $formHtmlElement.getElementsByTagName("select")) {
+    $fields.Add($newField.name, $newField.Value)
+  }
+
+  return $fields
 }
 
 Main
