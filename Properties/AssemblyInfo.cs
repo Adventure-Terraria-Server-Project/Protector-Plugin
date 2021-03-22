5ac4efd938ddf438ed4216bc7736f5e22b5d4f78
Stealownz <stealownz@gmail.com>
2021-03-22 23:04:57 +0800

    chore(version): tick plugin version 1.8.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("0d1df7a2-004f-4e62-830a-49477ea0b9be")]
 
-[assembly: AssemblyVersion("1.8.0.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
+[assembly: AssemblyVersion("1.8.1.0")]
+[assembly: AssemblyFileVersion("1.8.1.0")]
 
-[assembly: AssemblyInformationalVersion("1.8.0+7.Branch.master.Sha.a899868a96901a95430d6bedc84fc450ddec5a76")]
+[assembly: AssemblyInformationalVersion("1.8.1+1.Branch.master.Sha.5922d6ebc38bcd933c9ad3b8e776830b423dc7e9")]
