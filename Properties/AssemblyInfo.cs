ed60bb8f09091ad59617661be7a36623ba6afca3
Stealownz <stealownz@gmail.com>
2021-05-25 01:01:42 +0800

    chore(version): tick plugin version 1.9.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Protector Plugin")]
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("0d1df7a2-004f-4e62-830a-49477ea0b9be")]
 
-[assembly: AssemblyVersion("1.8.1.0")]
-[assembly: AssemblyFileVersion("1.8.1.0")]
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
 
-[assembly: AssemblyInformationalVersion("1.8.1+1.Branch.master.Sha.5922d6ebc38bcd933c9ad3b8e776830b423dc7e9")]
+[assembly: AssemblyInformationalVersion("1.9.0+2.Branch.master.Sha.3799b8b5a601334bee4a05119c094ddc6493dc94")]
